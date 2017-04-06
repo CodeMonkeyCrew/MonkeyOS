ca02cc39d273edb489f44eaaa80a6f76d0b6f112
laooola <patrick_schedler@hotmail.com>
2017-04-06 22:08:50 +0200

    remove debug folder

--- Debug/src/Drivers/GPIO-Driver.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-src/Drivers/GPIO-Driver.obj: ../src/Drivers/GPIO-Driver.c
-src/Drivers/GPIO-Driver.obj: ../src/Drivers/GPIO-Driver.h
-src/Drivers/GPIO-Driver.obj: ../src/Drivers/../HAL/GPIO-HAL.h
-
-../src/Drivers/GPIO-Driver.c: 
-../src/Drivers/GPIO-Driver.h: 
-../src/Drivers/../HAL/GPIO-HAL.h: 
--- Debug/src/HAL/GPIO-HAL.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-src/HAL/GPIO-HAL.obj: ../src/HAL/GPIO-HAL.c
-src/HAL/GPIO-HAL.obj: ../src/HAL/GPIO-HAL.h
-
-../src/HAL/GPIO-HAL.c: 
-../src/HAL/GPIO-HAL.h: 
