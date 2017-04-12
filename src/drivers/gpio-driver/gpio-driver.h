26fd0be2800645df4347fa952e191e50f71d8f12
nfe3303@students.fhv.at <nfe3303@students.fhv.at>
2017-04-12 09:06:53 +0200

    Folder restructure

--- src/drivers/gpio-driver/gpio-driver.h
+++ src/drivers/gpio-driver/gpio-driver.h
@@ -1,6 +1,6 @@
 #ifndef GPIO_DRIVER_H
 #define GPIO_DRIVER_H
-#include "../hal/gpio-hal.h"
+#include "gpio-hal/gpio-hal.h"
 
  //f.ex gpioPinNumber=GPIO_149, outputMode=0 (input), on=1 (turn on) Interface for IO-Handler, calls the following functions
 int writeGPIO(int gpioPinNumber, int outputMode, int on);
