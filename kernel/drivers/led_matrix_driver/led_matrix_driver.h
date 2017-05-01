a174d8b01c8c5f7e6a1c39ec9fca460d1198ee7b
nfe3303@students.fhv.at <nfe3303@students.fhv.at>
2017-05-01 18:53:28 +0200

    driver added, methodes defined

--- kernel/drivers/led_matrix_driver/led_matrix_driver.h
+++ kernel/drivers/led_matrix_driver/led_matrix_driver.h
@@ -1,13 +1,23 @@
-/*
- * led_matrix_driver.h
- *
- *  Created on: May 1, 2017
- *      Author: mod
- */
-
 #ifndef KERNEL_DRIVERS_LED_MATRIX_DRIVER_LED_MATRIX_DRIVER_H_
 #define KERNEL_DRIVERS_LED_MATRIX_DRIVER_LED_MATRIX_DRIVER_H_
+#include <inttypes.h>
 
+struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+}RGB;
+
+struct {
+    uint8_t y;
+    uint8_t x;
+}Coordinate;
+
+uint32_t init_matrix(const uint8_t gpio, const uint8_t[] gpio_pins);
+
+uint32_t draw_pixel(const struct Coordinate pos, const struct RGB rgb);
+
+uint32_t draw_rect(const struct Coordinate pos, const uint8_t length, const uint8_t height, const struct RGB rgb);
 
 
 #endif /* KERNEL_DRIVERS_LED_MATRIX_DRIVER_LED_MATRIX_DRIVER_H_ */
