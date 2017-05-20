6e7583d89191f505d1efbc907f6728d5eda89aaa
mondbaer <nfe3303@students.fhv.at>
2017-05-20 15:52:39 +0200

    Ascii header added

--- kernel/drivers/gpio_driver/gpiodriver.h
+++ kernel/drivers/gpio_driver/gpiodriver.h
@@ -1,6 +1,12 @@
 #ifndef KERNEL_DRIVERS_GPIO_DRIVER_GPIODRIVER_H_
 #define KERNEL_DRIVERS_GPIO_DRIVER_GPIODRIVER_H_
-
+/*  _____ _____ _____ ____    _____  _____  _______      ________ _____
+ * / ____|  __ \_   _/ __ \  |  __ \|  __ \|_   _\ \    / /  ____|  __ \
+ *| |  __| |__) || || |  | | | |  | | |__) | | |  \ \  / /| |__  | |__) |
+ *| | |_ |  ___/ | || |  | | | |  | |  _  /  | |   \ \/ / |  __| |  _  /
+ *| |__| | |    _| || |__| | | |__| | | \ \ _| |_   \  /  | |____| | \ \
+ * \_____|_|   |_____\____/  |_____/|_|  \_\_____|   \/   |______|_|  \_\
+ */
 void gpiodriver_init(void);
 
 #endif /* KERNEL_DRIVERS_GPIO_DRIVER_GPIODRIVER_H_ */
