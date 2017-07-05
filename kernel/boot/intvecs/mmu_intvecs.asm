6b5eaefc5cf833a2ad6805f26ee9ae577ebd3da6
Michael Troy <troy.michael@hotmail.com>
2017-07-05 10:09:43 +0200

    added intvecs table to use after mmu init - not used right now

---
+++ kernel/boot/intvecs/mmu_intvecs.asm
@@ -0,0 +1,18 @@
+	.global _ISR_UNDEF
+	.global _ISR_SWI
+	.global _ISR_PREFETCH
+	.global _ISR_ABORT
+	.global _ISR_UNUSED
+	.global _ISR_IRQ
+	.global _ISR_FIQ
+
+	.retain ".intvecs"
+	.sect ".intvecs"
+	.word 0
+	B _ISR_UNDEF
+	B _ISR_SWI
+	B _ISR_PREFETCH
+	B _ISR_ABORT
+	B _ISR_UNUSED
+	B _ISR_IRQ
+	B _ISR_FIQ
